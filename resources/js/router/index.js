be43dd57fdd43c56df475514251094b0b0623c43
oliver <kunhan945@gmail.com>
2022-07-28 22:07:01 +0800

    admin route

--- resources/js/router/index.js
+++ resources/js/router/index.js
@@ -2,9 +2,11 @@ import { createRouter, createWebHistory } from 'vue-router';
 
 const Home = () => import('../views/Home.vue');
 const Page = () => import('../views/Page.vue');
+const Admin = () => import('../views/Admin/index.vue');
 
 const routes = [
     { path: '/', name: 'Home', component: Home },
+    { path: '/admin', name: 'Admin', component: Admin }, // change route in production
     { path: '/page/:id', name: 'Page', component: Page },
 ];
 
