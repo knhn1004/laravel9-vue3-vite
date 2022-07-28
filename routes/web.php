123a062d260f38b871554c0dfc213b89dcf10ba2
oliver <kunhan945@gmail.com>
2022-07-28 21:16:16 +0800

    map all routes to vue (exclude /api)

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('{any}', function () {
     return view('storefront.index');
-})->name('storefront.index');
+})
+    // exclude /api/v1/ from the route
+    ->where('any', '^(?!api/).*$')
+    ->name('storefront.index');
