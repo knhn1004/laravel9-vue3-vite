c8e9812fbd22b10da7d4911f438af5fd2ddff39e
oliver <kunhan945@gmail.com>
2022-07-28 23:02:10 +0800

    test layer.msg

--- resources/js/views/Home.vue
+++ resources/js/views/Home.vue
@@ -1,8 +1,22 @@
 <template>
     <div class="layui-container">
         <lay-button-container>
-            <lay-button type="primary" size="lg">button 1</lay-button>
+            <lay-button type="primary" size="lg" @click="openMsg">button 1</lay-button>
             <lay-button type="primary" size="lg">button 2</lay-button>
         </lay-button-container>
     </div>
 </template>
+
+<script>
+import { layer } from '@layui/layui-vue'
+export default {
+    setup() {
+        const openMsg = function () {
+            layer.msg("普通消息", { time: 1000 })
+        }
+        return {
+            openMsg
+        }
+    }
+}
+</script>
